24aa76a7ce567d8ff0126e9da740ec3f766dbc97
ayoubbenchafai <ayoubbenchafai3@gmail.com>
2024-02-16 20:35:09 +0100

    update

--- push_swap_f/bonus/checker.c
+++ push_swap_f/bonus/checker.c
@@ -6,7 +6,7 @@
 /*   By: aben-cha <aben-cha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/08 22:01:36 by aben-cha          #+#    #+#             */
-/*   Updated: 2024/02/16 19:16:02 by aben-cha         ###   ########.fr       */
+/*   Updated: 2024/02/16 20:16:16 by aben-cha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,58 +29,35 @@ int instructions(t_stack **a,t_stack **b, char *s)
 {
     if(!s || !(*a))
         return 1;  
-    if(!strcmp(s, "sa\n")){
+    if(!strcmp(s, "sa\n"))
         sa(a);
-        display_a(*a);
-    }
-    else if(!strcmp(s, "sb\n")){
+    else if(!strcmp(s, "sb\n"))
         sb(b);
-        display_a(*a);
-    }
-    else if(!strcmp(s, "ss\n")){
+    else if(!strcmp(s, "ss\n"))
+    {
         ss(a,b);
         display_a(*a);
+        display_a(*b);
     }
-    else if(!strcmp(s, "pb\n")){
+    else if(!strcmp(s, "pb\n"))
         pb(a,b);
-        display_a(*a);
-    }
-    else if(!strcmp(s, "pa\n")){
+    else if(!strcmp(s, "pa\n"))
         pa(a,b);
-        display_a(*a);
-    }
-    else if(!strcmp(s, "ra\n")){
+    else if(!strcmp(s, "ra\n"))
         ra(a);
-        display_a(*a);
-    }
-    else if(!strcmp(s, "rb\n")){
+    else if(!strcmp(s, "rb\n"))
         rb(a);
-        display_a(*a);
-    }
-    else if(!strcmp(s, "rr\n")){
+    else if(!strcmp(s, "rr\n"))
         rr(a,b);
-        display_a(*a);
-    }
     else if(!strcmp(s, "rra\n"))
-    {
         rra(a);
-        display_a(*a);
-    }
     else if(!strcmp(s, "rrb\n"))
-    {
         rrb(a);
-        display_a(*a);
-    }
     else if(!strcmp(s, "rrr\n"))
-    {
         rrr(a,b);
-        display_a(*a);
-    }
     else
-    {
-        ft_putstr("Error\n");
-        return 1;
-    }
+        return (ft_putstr("Error\n") ,1);     
+    
     return (0);
 }
 int	ft_check(char *s, char c)
@@ -119,14 +96,17 @@ void display_b(t_stack *b)
     }
         printf("\n");
 }
-
+void f()
+{
+    system("leaks checker");
+}
 int main(int ac, char *av[])
 {
     t_stack *a;
     t_stack *b;
     char *s;
     int i;
-
+    atexit(f);
     i = 0;
     a = NULL;
     b = NULL;
@@ -136,23 +116,20 @@ int main(int ac, char *av[])
     {
         if(parsing(av[i], &a, ' '))
             return (1);
-        
     }
-    i = -1;
     while (1)
     {
         s = get_next_line(0);
         if(!s)
         {
             if(is_sorted(a) && !b)
-                return (free(s), ft_putstr("OK\n"), 0);
+                return ( ft_lstclear(&a),ft_putstr("OK\n"), 0);
             else
-                return (free(s), ft_putstr("KO\n"), 0);
+                return (ft_lstclear(&a),ft_lstclear(&b), ft_putstr("KO\n"), 0);
         }
         if(instructions(&a, &b, s))
-                return (free(s), 1);
+                return (free(s),ft_lstclear(&b), ft_lstclear(&a), 1);
         free(s);
     }
-    ft_lstclear(&a);
     return (0);
 }
